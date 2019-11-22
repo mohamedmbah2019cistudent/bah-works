b702ad67d4da5b717586e8bf036f676b9e8ed483
Mohamed Marifa Bah <mohamedmarifa@gmail.com>
2019-11-22 01:21:34 +0000

    Reinitial commit

--- bahwork/settings.py
+++ bahwork/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '733pr2gq_+=cf*d=j(oqwfrj%3j9au!q^=b^b^&c4hvqugni1%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
